e96ae8f56c641c62d92e30351135ecbc8b09851f
unx21 <84166927+unx21@users.noreply.github.com>
2021-07-20 05:11:50 +0800

    Add files via upload

---
+++ plugins/slime.js
@@ -0,0 +1,13 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './src/sound/slime.mp3'
+conn.sendFile(m.chat, vn, 'slime.mp3', null, m, true, {
+type: 'audioMessage', // paksa tanpa convert di ffmpeg
+ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+})
+}
+handler.customPrefix = /Rimuruu/
+handler.command = new RegExp
+module.exports = handler
