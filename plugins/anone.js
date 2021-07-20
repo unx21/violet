ed8ba1f0f3b25ed4448ac21408aff0989b37bd49
unx21 <84166927+unx21@users.noreply.github.com>
2021-07-20 22:14:41 +0800

    Update anone.js

--- plugins/anone.js
+++ plugins/anone.js
@@ -2,8 +2,8 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn = './src/sound/anone.mp3'
-conn.sendFile(m.chat, vn, 'anone.mp3', null, m, true, {
+let vn = './src/sound/Anone.mp3'
+conn.sendFile(m.chat, vn, 'Anone.mp3', null, m, true, {
 type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
