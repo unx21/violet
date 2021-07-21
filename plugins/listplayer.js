531d57585c65c0cc3841a7442a8d49d1b82f1513
unx21 <84166927+unx21@users.noreply.github.com>
2021-07-21 17:32:42 +0800

    Add files via upload

---
+++ plugins/listplayer.js
@@ -0,0 +1,22 @@
+/*
+   Silahkan Di Pakek
+   Tapi Bantu Rapihin :v
+   Buatan: Miaweers
+*/
+
+const { MessageType } = require('@adiwajshing/baileys')
+
+let handler = async (m, { conn, text }) => {
+  let player = global.player.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)
+conn.sendMessage(m.chat, `*「 List Player 」*` + `\n\n` + player.map(v => '❖ @' + v.replace(/@.+/, '')).join`\n`, MessageType.extendedText, { contextInfo: { mentionedJid: player } })
+}
+handler.help = ['listplayer']
+handler.tags = ['quest']
+handler.command = /^(listplayer)$/i
+handler.owner = false
+handler.group = true
+handler.private = false
+handler.player = true
+handler.register = true
+
+module.exports = handler
---
+++ plugins/quest.js
@@ -0,0 +1,54 @@
+let handler  = async (m, { conn, usedPrefix: _p  }) => {
+  conn.reply(m.chat, `
+╔═════════════════════╗
+┇                       *「 QUEST 」*
+┣ ┅ ━━━━━━━━━━━━━━━━━ ┅ ━
+║
+║ ❖  Kumpulkan Saldo Senilai 50 M
+║
+┣ ┅ ━━━━━━━━━━━━━━━━━ ┅ ━
+┇                      *「 HADIAH 」*
+┣ ┅ ━━━━━━━━━━━━━━━━━ ┅ ━
+║
+║ ❖  Posisi I = Extra Saldo 25 M
+║ ❖  Posisi II = Extra Saldo 20 M
+║ ❖  Posisi III = Extra Saldo 15 M
+║ ❖  Posisi IV = Extra Saldo 10 M
+║
+┣ ┅ ━━━━━━━━━━━━━━━━━ ┅ ━
+┇                    *「 RULES 」*
+┣ ┅ ━━━━━━━━━━━━━━━━━ ┅ ━
+║
+║ ❖  Quest Dibuka Setiap Hari Rabu dan Sabtu
+║ ❖  Untuk Mengikuti Quest Harap Mendaftar
+║ ❖  Admin Akan Memberikan Pegangan Sebesar 1 M
+║ ❖  Dilarang Meminta Saldo Dari Pemain Lain
+║
+┣ ┅ ━━━━━━━━━━━━━━━━━ ┅ ━
+┇                    *「 TIME 」*
+┣ ┅ ━━━━━━━━━━━━━━━━━ ┅ ━
+║
+║ ❖  Quest Dimulai Pukul 18.00 WIB
+║ ❖  Quest Berakhir Pukul 23.00 WIB
+║
+╚═════════════════════╝
+
+`.trim(), m)
+}
+handler.help = ['quest']
+handler.tags = ['quest']
+handler.command = /^(quest)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.player = true
+handler.group = true
+handler.private = false
+handler.register = true
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
