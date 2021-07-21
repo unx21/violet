62a87f5e9773f2b03b020457c3e472ef853ca95c
unx21 <84166927+unx21@users.noreply.github.com>
2021-07-22 00:04:55 +0800

    Update leave.js

--- plugins/leave.js
+++ plugins/leave.js
@@ -3,7 +3,7 @@ let handler = async function(m, { conn, args, isPrems, isOwner }) {
 const time = async (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-      this.fakeReply(m.chat, `*Bye Semuaa 🙂*`, '0@s.whatsapp.net', `${conn.user.name} Verified Bot`, 'status@broadcast')
+      this.fakeReply(m.chat, `*Bye Semua 🙂, Terimakasih Atas Kebersamaannya\n   Tertanda*\n\nヴァイオレット`, '0@s.whatsapp.net', `${conn.user.name} Verified Bot`, 'status@broadcast')
       await time(5000)
   await conn.groupLeave(m.key.remoteJid)
 }
