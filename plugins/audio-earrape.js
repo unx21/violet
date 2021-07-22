dee739be3898b94eab091f1efb7b2d1688955ed7
unx21 <84166927+unx21@users.noreply.github.com>
2021-07-22 22:27:14 +0800

    Update audio-earrape.js

--- plugins/audio-earrape.js
+++ plugins/audio-earrape.js
@@ -22,7 +22,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     }
 }
 handler.help = ['earrape']
-handler.tags = ['audio']
+handler.tags = ['Audio']
 handler.command = /^(earrape)$/i
 handler.register = true
 handler.limit = true
