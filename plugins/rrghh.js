9f7d2e2691f9e22b8efad0630a12bcf2b709f480
unx21 <84166927+unx21@users.noreply.github.com>
2021-07-22 23:42:08 +0800

    Update rrghh.js

--- plugins/rrghh.js
+++ plugins/rrghh.js
@@ -8,6 +8,6 @@ type: 'stickerMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /(a(rrgghh)/
+handler.customPrefix = /arrgghh|rrghh/
 handler.command = new RegExp
 module.exports = handler
