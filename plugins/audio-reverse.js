5f1151f542d72abcc2784a0c71df31b55cfc99f4
unx21 <84166927+unx21@users.noreply.github.com>
2021-07-22 22:37:12 +0800

    Update audio-reverse.js

--- plugins/audio-reverse.js
+++ plugins/audio-reverse.js
@@ -22,7 +22,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     }
 }
 handler.help = ['reverse']
-handler.tags = ['audio']
+handler.tags = ['Audio']
 handler.command = /^(reverse)$/i
 handler.register = true
 handler.limit = true
