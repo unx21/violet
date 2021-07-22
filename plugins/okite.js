5eb8281f6ab66011154657332ec8c7dadc6ece82
unx21 <84166927+unx21@users.noreply.github.com>
2021-07-23 05:25:10 +0800

    Update okite.js

--- plugins/okite.js
+++ plugins/okite.js
@@ -8,6 +8,6 @@ type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /Bangun cuy/
+handler.customPrefix = /bangun cuy|bangun oyy|bangun bang|bangun/
 handler.command = new RegExp
 module.exports = handler
