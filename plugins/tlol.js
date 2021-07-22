73054088d7f500afa27bc69d976ba5879973647d
unx21 <84166927+unx21@users.noreply.github.com>
2021-07-22 23:59:21 +0800

    Update tlol.js

--- plugins/tlol.js
+++ plugins/tlol.js
@@ -8,6 +8,6 @@ type: 'stickerMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /(Tolol|tolol/
+handler.customPrefix = /Tolol|tolol/
 handler.command = new RegExp
 module.exports = handler
