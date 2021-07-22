3c0ab31c74588fac7c7cf23ac03d2ee90bd984e9
unx21 <84166927+unx21@users.noreply.github.com>
2021-07-22 22:23:17 +0800

    Update absen-cekabsen.js

--- plugins/absen-cekabsen.js
+++ plugins/absen-cekabsen.js
@@ -26,7 +26,7 @@ ${list}
 Ini dibuat oleh _ Ariffb_`, m, { contextInfo: { mentionedJid: absen } })
 }
 handler.help = ['cekabsen']
-handler.tags = ['absen']
+handler.tags = ['Absen']
 handler.command = /^cekabsen$/i
 handler.group = true
 handler.register = true
