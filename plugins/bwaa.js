7cf448609c7789f05ac0a66790416ff1f42974de
unx21 <84166927+unx21@users.noreply.github.com>
2021-07-22 23:25:38 +0800

    Update bwaa.js

--- plugins/bwaa.js
+++ plugins/bwaa.js
@@ -8,6 +8,6 @@ type: 'stickerMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /(bwaa(hayolohh)|mo ngapain b[wa]ng)/
+handler.customPrefix = /bwaa hayolohh|mo ngapain bang/
 handler.command = new RegExp
 module.exports = handler
