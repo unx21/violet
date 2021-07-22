1b5d6c0187b9f3a5c7f141a67baf200a893642b4
unx21 <84166927+unx21@users.noreply.github.com>
2021-07-22 23:40:25 +0800

    Update pyamu.js

--- plugins/pyamu.js
+++ plugins/pyamu.js
@@ -8,6 +8,6 @@ type: 'stickerMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /Punya gw|punya gw/
+handler.customPrefix = /Punya gw|punya gw|Punya mu|punya mu/
 handler.command = new RegExp
 module.exports = handler
