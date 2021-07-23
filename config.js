0f9dcb5da85465f30f34133c83bc6ff1ccd14bb1
unx21 <84166927+unx21@users.noreply.github.com>
2021-07-23 22:16:52 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['62887436568613','6289524339740'] // Put your number here
+global.owner = ['62887436568613'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = ['62887436568613'] // Premium user has unlimited limit
 global.player = ['62887436568613']
