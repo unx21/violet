f710e7a8ba5b699bb1f37e4d980067babe7b94b1
unx21 <84166927+unx21@users.noreply.github.com>
2021-07-24 18:31:22 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -54,8 +54,8 @@ const defaultMenu = {
 ┃
 ┃ ❖ *Uptime:* _%uptime_ (%muptime)
 ┃ ❖ *Database:* %rtotalreg of %totalreg
-┃ ❖ *Contact:*
-┃     [62887436568613]
+┃ ❖ *About Me :*
+┃  _*https://bit.ly/unxzx_*
 ┗ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
 %readmore
 ┏ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
