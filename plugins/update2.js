148f68160625cf2d89fa7388c4a9d9fe2ddf1879
unx21 <84166927+unx21@users.noreply.github.com>
2021-07-26 01:12:24 +0800

    Update update2.js

--- plugins/update2.js
+++ plugins/update2.js
@@ -5,7 +5,7 @@ const { join } = require('path')
 
 let confirmation = {}
 let repository = 'unx21/violet'
-let branch = 'master'
+let branch = 'main'
 
 async function handler(m, { text }) {
     let res = await fetch(`https://raw.githubusercontent.com/${repository}/${branch}/${text}`)
