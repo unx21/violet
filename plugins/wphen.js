60ceca079f6bf5fab47ae9960d2c1ae4433e26fa
unx21 <84166927+unx21@users.noreply.github.com>
2021-07-29 11:49:13 +0800

    Update wphen.js

--- plugins/wphen.js
+++ plugins/wphen.js
@@ -1,23 +1,36 @@
-const { MessageType } = require('@adiwajshing/baileys')
-const fetch = require('node-fetch')
-
-let handler = async (m, { conn }) => {
-    try {
-      await m.reply(global.wait)
-        let res = await fetch(global.API('xteam', '/randomimage/hentai', {}, 'APIKEY'))
-        let img = await res.buffer()
-        conn.sendMessage(m.chat, img, MessageType.image, {
-            quoted: m, caption: '*©ヴァイオレット*'
-        })
-    } catch (e) {
-        console.log(e)
-        throw '_*Owner belum membayar tagihan fitur ini*_'
-    }
-}
-handler.help = ['wphentai']
-handler.tags = ['weebs']
-handler.command = /^(wallpaper|wp)hentai$/i
-handler.premium = true
+let handler = async m => m.reply(`
+┏ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
+┇       *「 PREMIUM 」*
+┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
+┃
+┃ ❖ Limit Tak Terbatas
+┃ ❖ Semua Fitur Terbuka
+┃ ❖ Private Chat Bot
+┃ ❖ Undang Bot Ke Group
+┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
+┃ Mau Jadi Premium?
+┃ 
+┃ Harga Premium
+┃ ❖ 5k/Bulan
+┃
+┃ Invite Bot Ke Group 
+┃ ❖ 15k/Bulan
+┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
+┃ 
+┃ Owner Akan Mengembalikan Sisa Premium
+┃ Via Pulsa Apabila Bot Telah Berhenti Permanent
+┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
+┃ Silahkan Hubungi Owner
+┃ Untuk Menjadi Premium User
+┃ 
+┃ Untuk Pembayaran Silahkan 
+┃ cek di #donasi
+┗ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['premium']
+handler.tags = ['premium']
+handler.command = /^premium$/i
+handler.group = false
 handler.register = true
 
 module.exports = handler
