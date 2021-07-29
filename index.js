b21107dbaedb407098134d003446b540c2e17d34
unx21 <84166927+unx21@users.noreply.github.com>
2021-07-29 22:09:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('Lightweight\nU n x  Loyal Couple', {
+CFonts.say('Violet\nU n x  Loyal Couple', {
   font: 'chrome',
   align: 'center',
   gradient: ['cyan', 'magenta']
