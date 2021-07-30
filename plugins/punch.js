16ab753f3ec279868f1da5b8ab36c3e34ae2333f
unx21 <84166927+unx21@users.noreply.github.com>
2021-07-30 17:39:33 +0800

    Update punch.js

--- plugins/punch.js
+++ plugins/punch.js
@@ -3,6 +3,7 @@ const { sticker } = require('../lib/sticker')
 const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn}) => {
+await m.reply(global.wait)
   try {
   let res = await fetch('https://neko-love.xyz/api/v1/punch')
   let json = await res.json()
