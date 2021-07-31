8589389f14070a1c1d7412c6977ece4e92777212
unx21 <84166927+unx21@users.noreply.github.com>
2021-07-31 20:36:54 +0800

    Update addprems.js

--- plugins/addprems.js
+++ plugins/addprems.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn }) => {
     else who = m.chat
     if (global.prems.includes(who.split`@`[0])) throw 'Sudah premium!'
     global.prems.push(`${who.split`@`[0]}`)
-    conn.reply(m.chat, `Hai, @${who.split`@`[0]}. Kamu sudah premium!`, m, {
+    conn.reply(m.chat, `Hai, @${who.split`@`[0]}. Kamu sudah premium, Jangan lupa untuk klaim *Gift*`, m, {
         contextInfo: {
             mentionedJid: [who]
         }
