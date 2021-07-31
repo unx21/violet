7467cf504fe71b6a7f1c41fcec1facf783e60689
unx21 <84166927+unx21@users.noreply.github.com>
2021-08-01 05:35:49 +0800

    Update tebaklirik.js

--- plugins/tebaklirik.js
+++ plugins/tebaklirik.js
@@ -1,5 +1,5 @@
-//by BochilGaming
-//https://github.com/BochilGaming/
+//by unx
+//https://github.com/unx21/
 
 let fetch = require('node-fetch')
 
@@ -8,7 +8,7 @@ let poin = 2500
 let handler  = async (m, { conn, usedPrefix }) => {
     conn.tebaklirik = conn.tebaklirik ? conn.tebaklirik : {}
     let id = m.chat
-    if (id in conn.caklontong) {
+    if (id in conn.tebaklirik) {
         conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebaklirik[id][0])
         throw false
     }
