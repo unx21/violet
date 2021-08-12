bf1f116ecbc6058487aaa4295fedc18a7435ff49
unx ? <84166927+unx21@users.noreply.github.com>
2021-08-12 11:31:08 +0800

    Update tagowner2.js

--- plugins/tagowner2.js
+++ plugins/tagowner2.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text }) => {
   let name = ('62887436568613@s.whatsapp.net')
   let panggil = `
-  Master Sedang Tidur Saat Ini, Mohon Untuk Tidak Mengganggu
+  Jangan Tag Master\n\nSaya Tidak Suka Jika Master Saya Di Tag Oleh Anda
   `.trim()
   let mentionedJid = [name]
  conn.reply(m.chat, panggil, m, { contextInfo: { mentionedJid }})
