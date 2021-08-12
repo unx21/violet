95ccccd9db3832774e85e5cf95345c333a1706d7
unx ? <84166927+unx21@users.noreply.github.com>
2021-08-12 11:35:06 +0800

    Update and rename wife.js to botchat13.js

--- plugins/botchat13.js
+++ plugins/botchat13.js
@@ -8,6 +8,6 @@ type: 'stickerMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /Huuu istrinya kartun/
+handler.customPrefix = /huu istrinya kartun/
 handler.command = new RegExp
 module.exports = handler
