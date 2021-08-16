c75d092eddf3d82c04d1cf7626bd3070e9664116
unx ? <84166927+unx21@users.noreply.github.com>
2021-08-16 19:42:43 +0800

    Update botchat.js

--- plugins/botchat.js
+++ plugins/botchat.js
@@ -1,6 +1,6 @@
 let handler  = async (m, { conn }) => {
 let name = conn.getName(m.sender)
-let pesan = `Hai ${name}\n\nKetik .menu atau .help untuk melihat semua fitur`
+let pesan = `Hai ${name}\n\nKetik ${usedPrefix}menu atau ${usedPrefix}help untuk melihat semua fitur`
 conn.reply(m.chat, pesan, m)
 }
 handler.customPrefix = /Bot/
