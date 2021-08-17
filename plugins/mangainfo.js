a0a14faae245216fea61076f483095cb34119a81
unx ? <84166927+unx21@users.noreply.github.com>
2021-08-17 21:21:56 +0800

    Create mangainfo.js

---
+++ plugins/mangainfo.js
@@ -0,0 +1,23 @@
+let fetch = require('node-fetch')
+let handler = async(m, { conn, text }) => {
+await m.reply(global.wait)
+  if (!text) throw `Masukkan query!`
+  let res = await fetch(global.API('https://api.jikan.moe', '/v3/search/manga', { q: text }))
+  if (!res.ok) throw await res.text()
+  let json = await res.json()
+  let { title, synopsis, chapters, url, volumes, score, image_url } = json.results[0]
+let mangaingfo = `*Title:* ${title}
+*Chapters:* ${chapters}
+*Volumes:* ${volumes}
+*Score:* ${score}
+*Synopsis:* ${synopsis}
+*Link*: ${url}`
+  conn.sendFile(m.chat, image_url, '', mangaingfo, m)
+}
+handler.help = ['manga <judul>']
+handler.tags = ['weebs']
+handler.command = /^(mangainfo)$/i
+handler.register = true
+handler.limit = true
+//Made by Ftwrr
+module.exports = handler
