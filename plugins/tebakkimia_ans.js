cf3cc89ded8aaff1b76a273051dd81870a64369c
unxzx <84166927+unx21@users.noreply.github.com>
2021-08-28 20:20:34 +0800

    Update tebakkimia_ans.js

--- plugins/tebakkimia_ans.js
+++ plugins/tebakkimia_ans.js
@@ -1,14 +1,14 @@
 let handler = m => m
 handler.before = async function (m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*teki/i.test(m.quoted.text)) return !0
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*tmhint/i.test(m.quoted.text)) return !0
     conn.tebakkimia = conn.tebakkimia ? conn.tebakkimia : {}
     if (!(id in conn.tebakkimia)) return m.reply('Soal itu telah berakhir')
     if (m.quoted.id == conn.tebakkimia[id][0].id) {
         let json = JSON.parse(JSON.stringify(conn.tebakkimia[id][1]))
         // m.reply(JSON.stringify(json, null, '\t'))
         if (m.text.toLowerCase() == json.name.toLowerCase().trim()) {
-            global.db.data.users[m.sender].exp += conn.tebakkimia[id][2]
+            global.DATABASE.data.users[m.sender].uang += conn.tebakkimia[id][2]
             m.reply(`*Benar!*\n+${conn.tebakkimia[id][2]} XP`)
             clearTimeout(conn.tebakkimia[id][3])
             delete conn.tebakkimia[id]
