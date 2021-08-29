209bc3789dd53b8cd2c08c1de369b0e519a98c88
unxzx <84166927+unx21@users.noreply.github.com>
2021-08-29 23:08:57 +0800

    Update botchat.js

--- plugins/botchat.js
+++ plugins/botchat.js
@@ -1,6 +1,6 @@
 let handler  = async (m, { conn }) => {
 let name = conn.getName(m.sender)
-let pesan = `Hai ${name}\n\nKetik ${usedPrefix}menu atau ${usedPrefix}help untuk melihat semua fitur`
+let pesan = `Hai ${name}, Saya disini..`
 conn.reply(m.chat, pesan, m)
 }
 handler.customPrefix = /Bot/
