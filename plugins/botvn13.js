67d2f6f51a9e9c63669224c1b791a0f28c51f1c6
unxzx <84166927+unx21@users.noreply.github.com>
2021-08-31 06:22:24 +0800

    Add files via upload

---
+++ plugins/botvn13.js
@@ -0,0 +1,13 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn ='src/sound/ahoy.mp3'
+conn.sendFile(m.chat, vn, 'ahoy.mp3', null, m, true, {
+type: 'audioMessage', // paksa tanpa convert di ffmpeg
+ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+})
+}
+handler.customPrefix = /Ahoyy|Ahoy/
+handler.command = new RegExp
+module.exports = handler
