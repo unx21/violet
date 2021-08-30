42344c4b8a49d234d84d8f77698ec0842f0e29b3
Rizxyu <88314302+Rizxyu@users.noreply.github.com>
2021-08-30 21:16:05 +0700

    Create Delsesittt.js

---
+++ plugins/Delsesittt.js
@@ -0,0 +1,24 @@
+let handler  = async (m, { conn, text }) => {
+	
+conn.game = conn.game ? conn.game : {}
+try {
+If ( conn.game[m.sender] == true ) {
+delete conn.game[m.seder]
+conn.reply( m.chat, `*[ 🎳 TicTacToe Games ]*
+session ttt berhasil dihapus`,
+m)
+} else if ( conn.game[m.sender == false) {
+m.reply(`lu g punya sesi tictactoe paok`)
+}
+} catch(e) {
+m.reply(`eror`)
+}
+	
+handler.help = ['delsesittt']
+handler.tags = ['game']
+handler.command = /^(delsesittt|dellsesitt)$/i
+
+handler.register = true
+handler.fail = null
+
+module.exports = handler
