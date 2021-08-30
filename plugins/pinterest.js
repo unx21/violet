f989ae0ee57c44b56a77f4832f36e2e12249302c
unxzx <84166927+unx21@users.noreply.github.com>
2021-08-30 22:34:14 +0800

    Update pinterest.js

--- plugins/pinterest.js
+++ plugins/pinterest.js
@@ -1,22 +1,53 @@
-let fetch = require('node-fetch')
-let handler = async(m, { conn, text, usedPrefix, command }) => {
-	await m.reply(global.wait)
-  if (!text) throw `Contoh: ${usedPrefix + command} minecraft`
-  let res = await fetch(global.API('zeks', '/api/pinimg', {
-    q: encodeURI(text)
-  }, 'apikey'))
-  if (!res.ok) throw await `${res.status} ${res.statusText}`
-  let json = await res.json()
-  if (!json.status) throw json
-  let pint = json.data[Math.floor(Math.random() * json.data.length)];
-  conn.sendFile(m.chat, pint, '', `
-*Hasil pencarian*
+let imageToBase64 = require('image-to-base64');
+let axios = require("axios");
+let handler = async(m, { conn, text }) => {
+
+if (!text) return conn.reply(m.chat, 'Harap masukan query!', m)
+
+let url = "https://fdciabdul.tech/api/pinterest/?keyword=" + text;
+let str = `
+Hasil Pencarian :
+
 ${text}
-`.trim(), m)
+`.trim()
+
+await m.reply(global.wait)
+axios.get(url)
+.then((result) => {
+let b = JSON.parse(JSON.stringify(result.data));
+let text = b[Math.floor(Math.random() * b.length)];
+imageToBase64(text) // Path to the image
+.then(
+(response) => {
+let buf = Buffer.from(response, 'base64'); // Ta-da
+
+conn.sendFile(m.chat, buf, 'foto.jpg', str, m)
+        }
+    )
+    .catch(
+        (error) => {
+            console.log(error); // Logs an error if there was one
+        }
+    )
+
+});
 }
-handler.help = ['pinterest <keyword>']
-handler.tags = ['internet','premium']
+
+handler.help = ['pinterest <query>']
+handler.tags = ['internet']
 handler.command = /^(pinterest)$/i
+handler.owner = false
+handler.mods = false
 handler.premium = true
+handler.group = false
+handler.private = false
+handler.register = true
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+handler.limit = false
 
 module.exports = handler
