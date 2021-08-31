2aef82a9ccca853ae62f6e790dabd7f1fd323324
unxzx <84166927+unx21@users.noreply.github.com>
2021-08-31 20:33:46 +0800

    Update wm.js

--- plugins/wm.js
+++ plugins/wm.js
@@ -1,38 +1,25 @@
-const uploadImage = require('../lib/uploadImage') 
-const { sticker } = require('../lib/sticker')
 const { MessageType } = require('@adiwajshing/baileys')
-
+const { sticker } = require('../lib/sticker')
 let handler = async (m, { conn, text }) => {
- try {
-  let q = m.quoted ? m.quoted : m
-  let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw 'Tidak ada foto'
-  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
-  let img = await q.download()
-  let url = await uploadImage(img)
-  let wanted = `https://api.dhamzxploit.my.id/api/canvas/wanted?url=${url}`
-  let stiker = await sticker(null, wanted, 'Wanted', 'Violet')
-  conn.sendMessage(m.chat, stiker, MessageType.sticker, {
-    quoted: m
-  })
- } catch (e) {
-   m.reply('Conversion Failed')
+  let stiker = false
+  try {
+    let [packname, ...author] = text.split('|')
+    author = (author || []).join('|')
+    let mime = m.quoted.mimetype || ''
+    if (!/webp/.test(mime)) throw 'Reply sticker!'
+    let img = await m.quoted.download()
+    stiker = await sticker(img, false, packname || '', author || '')
+  } finally {
+    if (stiker) conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+      quoted: m
+    })
+    else throw 'Conversion failed'
   }
 }
-handler.help = ['wanted']
+handler.help = ['wm <packname>|<author>']
 handler.tags = ['sticker']
-handler.command = /^wanted$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
+handler.command = /^wm$/i
+handler.premium = true
 handler.register = true
-handler.limit = true
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
 
 module.exports = handler
