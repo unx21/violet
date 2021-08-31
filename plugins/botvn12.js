58d6f48ddeddd34c4a371b0bed72e0c843e67aad
unxzx <84166927+unx21@users.noreply.github.com>
2021-08-31 16:09:33 +0800

    Update botvn12.js

--- plugins/botvn12.js
+++ plugins/botvn12.js
@@ -2,7 +2,7 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn ='src/sound/horny.mp3'
+let vn = './src/sound/horny.mp3'
 conn.sendFile(m.chat, vn, 'horny.mp3', null, m, true, {
 type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
