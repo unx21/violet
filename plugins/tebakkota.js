f5ab925ddaa6caa318d273c54d1287fd73a0caff
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-02 00:05:38 +0800

    :v:

--- plugins/tebakkota.js
+++ plugins/tebakkota.js
@@ -1,7 +1,9 @@
-let fetch = require('node-fetch')
+//Fitur nya w disabled, alasannya karena saya.... *_Yah itu_*//
 
+
+let fetch = require('node-fetch')
 let timeout = 120000
-let poin = 2500
+let poin = 500
 let handler = async (m, { conn, usedPrefix }) => {
     conn.tebakkota = conn.tebakkota ? conn.tebakkota : {}
     let id = m.chat
@@ -9,29 +11,34 @@ let handler = async (m, { conn, usedPrefix }) => {
         conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakkota[id][0])
         throw false
     }
-    let res = await fetch(API('dhnjing', '/fun/tebakkota'))
-    if (res.status !== 200) throw await res.text()
-    let json = await res.json()
-    if (json.status != 200) throw json
+    let res = await fetch(API('https://dhnjing.xyz/fun/tebakkota?apikey=ea9dbc3e49b85e'))
+    if (!res.status) throw eror
+    let result = await res.json()
+    if (result.status != 200) throw json
+    let json = result.result
     let caption = `
-${json.soal}
-Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}tekohint untuk bantuan
-Bonus: Rp${poin}
-`.trim()
-    conn.tebakkota[id] = [
-        await conn.reply(m.chat, caption, m),
-        json, poin,
-        setTimeout(() => {
-            if (conn.tebakkota[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.name}*`, conn.tebakkota[id][0])
-            delete conn.tebakkota[id]
-        }, timeout)
-    ]
-}
-handler.help = ['tebakkota']
-handler.tags = ['game']
-handler.command = /^tebakkota/i
-handler.limit = true
-handler.register
+*「 Tebak Kota 」*
 
-module.exports = handler
+${json.soal}
+
+Timeout *${(timeout / 1000).toFixed(2)} detik*
+Ketik ${usedPrefix}teko untuk bantuan
+Bonus Rp${poin}
+`.trim()
+ conn.tebakkota[id] = [
+      await conn.reply(m.chat, caption, m),
+      json, poin,
+      setTimeout(() => {
+        if (conn.tebakkota[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, conn.tebakkota[id][0])
+        delete conn.tebakkota[id]
+      }, timeout)
+    ]
+  }
+  handler.help = ['tebakkota']
+  handler.tags = ['game']
+  handler.command = /^tebakkota/i
+  handler.register = true
+  handler.disabled = true
+  handler.limit = true
+  
+  module.exports = handler
