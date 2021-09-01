a6d47d4e804986accf40a057d09757bbbfc5712c
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-02 00:10:19 +0800

    Update tebakkota_hint.js

--- plugins/tebakkota_hint.js
+++ plugins/tebakkota_hint.js
@@ -3,8 +3,10 @@ let handler = async (m, { conn }) => {
     let id = m.chat
     if (!(id in conn.tebakkota)) throw false
     let json = conn.tebakkota[id][1]
-    m.reply('```' + json.name.replace(/[bcdfghjklmnpqrstvwxyz]/g, '_') + '```')
+    let nya = json.jawaban
+    let nyanya = nya.replace(/[BCDFGHJKLMNPQRSTVWXYZ]/g, '_')
+    m.reply('```' + nyanya + '```')
 }
-handler.command = /^tekohint$/i
+handler.command = /^teko$/i
 handler.limit = true
 module.exports = handler
