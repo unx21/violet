8a4e3eaf069932be2322f20a51e782944a103edd
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-02 00:49:24 +0800

    Update tebakkota_ans.js

--- plugins/tebakkota_ans.js
+++ plugins/tebakkota_ans.js
@@ -1,22 +1,22 @@
+//DIJIPLAK PENUH CINTA OLEH SAYA//
 
 let handler = m => m
 handler.before = async function (m) {
   let id = m.chat
-  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/「 Tebak Kota 」/i.test(m.quoted.text)) return !0
+  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/「 Tebak Kota 」/i.test(m.quoted.text)) return
   conn.tebakkota = conn.tebakkota ? conn.tebakkota : {}
-  if (!(id in conn.tebakkota)) return m.reply('Soal itu telah berakhir')
+  if (!(id in conn.tebakkota)) throw 'Soal itu telah berakhir!'
   if (m.quoted.id == conn.tebakkota[id][0].id) {
     let json = JSON.parse(JSON.stringify(conn.tebakkota[id][1]))
     // m.reply(JSON.stringify(json, null, '\t'))
-    if (' '+m.text.toUpperCase() == json.result.jawaban.toUpperCase()) {
+    if (m.text.toLowerCase() == json.jawaban.toLowerCase()) {
       global.DATABASE._data.users[m.sender].uang += conn.tebakkota[id][2]
       m.reply(`*Benar!*\n+Rp${conn.tebakkota[id][2]}`)
       clearTimeout(conn.tebakkota[id][3])
       delete conn.tebakkota[id]
-    } else if (m.text.toUpperCase().endsWith(json.jawaban.split` `[1])) m.reply(`*Dikit Lagi!*`)
+    } else if (m.text.toLowerCase().endsWith(json.jawaban.split` `[1])) m.reply(`*Dikit Lagi!*`)
     else m.reply(`*Salah!*`)
   }
-  return !0
 }
 handler.exp = 0
 
