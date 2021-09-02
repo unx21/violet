d4d1f232051157e2a7b3889825c7ced6a6c6697d
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-02 13:22:38 +0800

    updd

--- plugins/caklontong.js
+++ plugins/caklontong.js
@@ -1,42 +1,38 @@
-//by BochilGaming
-//https://github.com/BochilGaming/
+//Thanks To BochilGaming//
 
 let fetch = require('node-fetch')
-
 let timeout = 120000
 let poin = 2500
-let handler  = async (m, { conn, usedPrefix }) => {
+let handler = async (m, { conn, usedPrefix }) => {
     conn.caklontong = conn.caklontong ? conn.caklontong : {}
     let id = m.chat
     if (id in conn.caklontong) {
         conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.caklontong[id][0])
         throw false
     }
-    let res = await fetch('http://94.130.142.91:25621/caklontong')
-    let json = await res.json()
+    let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/caklontong.json')).json()
+    let json = src[Math.floor(Math.random() * src.length)]
     let caption = `
 *「 Cak Lontong 」*
+${json.soal}
 
-Soal: *${json.result.soal}*
-
-Timeout: *${(timeout / 1000).toFixed(2)} detik*
-Ketik *${usedPrefix}clhint* untuk bantuan
-Bonus: +Rp${poin}
+Timeout *${(timeout / 1000).toFixed(2)} detik*
+Ketik ${usedPrefix}clhint untuk bantuan
+Bonus: ${poin}
 `.trim()
-    conn.caklontong[id] = [
-      await conn.reply(m.chat,  caption, m),
+  conn.caklontong[id] = [
+      await conn.reply(m.chat, caption, m),
       json, poin,
       setTimeout(() => {
-        if (conn.caklontong[id]) conn.reply(m.chat, `Waktu habis!\n${json.result.deskripsi}`, conn.caklontong[id][0])
+        if (conn.caklontong[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, conn.caklontong[id][0])
         delete conn.caklontong[id]
       }, timeout)
     ]
   }
-handler.help = ['caklontong']
-handler.tags = ['game']
-handler.command = /^caklontong/i
-handler.register = true
-
-handler.limit = true
+  handler.help = ['caklontong']
+  handler.tags = ['game']
+  handler.command = /^caklontong/i
+  handler.register = true
+  handler.limit = true
   
-module.exports = handler
+  module.exports = handler
