ef0546210ca537f87080289d22f1311dea420e66
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-02 13:24:16 +0800

    Update caklontong_hint.js

--- plugins/caklontong_hint.js
+++ plugins/caklontong_hint.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
     let id = m.chat
     if (!(id in conn.caklontong)) throw false
     let json = conn.caklontong[id][1]
-    let nya = json.result.jawaban
+    let nya = json.jawaban
     let nyanya = nya.replace(/[bcdfghjklmnpqrstvwxyz]/g, '_')
     m.reply('```' + nyanya + '```')
 }
