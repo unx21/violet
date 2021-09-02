3f69a78646f05868a2e996856c2d8c0b642c4e34
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-02 13:37:34 +0800

    Update and rename tebaklirik_answer.js to tebaklirik_ans.js

--- plugins/tebaklirik_ans.js
+++ plugins/tebaklirik_ans.js
@@ -1,5 +1,4 @@
-//by unx
-//https://github.com/unx21/
+//Thanks To BochilGaming//
 
 let handler = m => m
 handler.before = async function (m) {
@@ -10,12 +9,12 @@ handler.before = async function (m) {
   if (m.quoted.id == conn.tebaklirik[id][0].id) {
     let json = JSON.parse(JSON.stringify(conn.tebaklirik[id][1]))
     // m.reply(JSON.stringify(json, null, '\t'))
-    if (m.text.toLowerCase() == json.result.jawaban.toLowerCase()) {
+    if (m.text.toLowerCase() == json.jawaban.toLowerCase()) {
       global.DATABASE._data.users[m.sender].uang += conn.tebaklirik[id][2]
       m.reply(`*Benar!*\n+Rp${conn.tebaklirik[id][2]}`)
       clearTimeout(conn.tebaklirik[id][3])
       delete conn.tebaklirik[id]
-    } else if (m.text.toLowerCase().endsWith(json.result.jawaban.split` `[1])) m.reply(`*Dikit Lagi!*`)
+    } else if (m.text.toLowerCase().endsWith(json.jawaban.split` `[1])) m.reply(`*Dikit Lagi!*`)
     else m.reply(`*Salah!*`)
   }
 }
