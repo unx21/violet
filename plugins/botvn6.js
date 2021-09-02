b0a67ff7f2c9ffe2138e615acf6b293e052e6417
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-02 21:39:45 +0800

    Update botvn6.js

--- plugins/botvn6.js
+++ plugins/botvn6.js
@@ -8,6 +8,6 @@ type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /Hentai/
+handler.customPrefix = /Hentai|hentai/
 handler.command = new RegExp
 module.exports = handler
