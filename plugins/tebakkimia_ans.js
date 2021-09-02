e9a13592f15efc501e7b9b988ac031d9a9bcdd63
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-02 11:02:36 +0800

    Update tebakkimia_ans.js

--- plugins/tebakkimia_ans.js
+++ plugins/tebakkimia_ans.js
@@ -9,7 +9,7 @@ handler.before = async function (m) {
         // m.reply(JSON.stringify(json, null, '\t'))
         if (m.text.toLowerCase() == json.name.toLowerCase().trim()) {
             global.DATABASE.data.users[m.sender].uang += conn.tebakkimia[id][2]
-            m.reply(`*Benar!*\n+${conn.tebakkimia[id][2]} XP`)
+            m.reply(`*Benar!*\n+${conn.tebakkimia[id][2]}`)
             clearTimeout(conn.tebakkimia[id][3])
             delete conn.tebakkimia[id]
         } else if (m.text.toLowerCase().endsWith(json.name.split` `[1])) m.reply(`*Dikit Lagi!*`)
