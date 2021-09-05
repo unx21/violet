edee683d61380e9f6a9dec66d1f0ff7f884fc680
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-05 09:36:06 +0800

    Update anmovie.js

--- plugins/anmovie.js
+++ plugins/anmovie.js
@@ -1,56 +1,59 @@
-let handler = async m => m.reply(`
+let handler = async (m) => {
+  
+let violet =  './src/photo/VGarden.png'
+let teks = `
 ┏ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
 ┇          *「 Link Anime Movie 」*
-┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
+┣ ┅ ━━━━━━━━━━━
 ┃    *Kualitas 480 Tertera*
 ┃    *Kualitas 720 Tertera*
 ┃    *Kualitas 1080 Tertera*
-┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
+┣ ┅ ━━━━━━━━━━━
 ┇         *「Violet Evergarden : The Movie 」*
-┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
+┣ ┅ ━━━━━━━━━━━
 ┃         Kualitas 480
 ┃ ~ https://www.mp4upload.com/hjbxis14z3q9
 ┃          Kualitas 720
 ┃ ~ https://www.mp4upload.com/4c75b84c8oks
-┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
+┣ ┅ ━━━━━━━━━━━
 ┇ *「 Demon Slayer The Movie : Mugen Ressha-Hen 」*
-┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
+┣ ┅ ━━━━━━━━━━━
 ┃          Kualitas 480
 ┃ ~ https://uptobox.com/81ffkmkcq0lm
 ┃          Kualitas 720
 ┃ ~ https://uptobox.com/b5i1eeg83odn
 ┃          Kualitas 1080
 ┃ ~ https://uptobox.com/a389ej9tc2ve
-┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
-┇     *「 Koe no Katachi 」*
-┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
+┣ ┅ ━━━━━━━━━━━
+┇   *「 Koe no Katachi 」*
+┣ ┅ ━━━━━━━━━━━
 ┃          Kualitas 480
 ┃ ~ https://uptobox.com/alyiqro9urni
 ┃          Kualitas 720
 ┃ ~ https://uptobox.com/psa58v1zby9t
 ┃          Kualitas 1080
 ┃ ~ https://uptobox.com/di6923f37jjc
-┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
-┇      *「 Kimi no Na Wa 」*
-┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
+┣ ┅ ━━━━━━━━━━━
+┇   *「 Kimi no Na Wa 」*
+┣ ┅ ━━━━━━━━━━━
 ┃          Kualitas 480
 ┃ ~ https://uptobox.com/8lg6fa3tqsf8
 ┃          Kualitas 720
 ┃ ~ https://uptobox.com/lg4wgm6i1ftl
 ┃          Kualitas 1080
 ┃ ~ https://uptobox.com/4g4far8u5zzn
-┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
-┇      *「 Tenki no Ko 」*
-┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
+┣ ┅ ━━━━━━━━━━━
+┇   *「 Tenki no Ko 」*
+┣ ┅ ━━━━━━━━━━━
 ┃          Kualitas 480
 ┃ ~ https://uptobox.com/jurgn2bn007u
 ┃          Kualitas 720
 ┃ ~ https://uptobox.com/3hdjd56gy2g2
 ┃          Kualitas 1080
 ┃ ~ https://uptobox.com/mq15rgxabmzl
-┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
-┇     *「 Byousoku 5 Centimeter 」*
-┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
+┣ ┅ ━━━━━━━━━━━
+┇   *「 Byousoku 5 Centimeter 」*
+┣ ┅ ━━━━━━━━━━━
 ┃          Kualitas 480
 ┃ ~ https://uptobox.com/ba2uumhzhx1j
 ┃          Kualitas 720
@@ -58,7 +61,7 @@ let handler = async m => m.reply(`
 ┃          Kualitas 1080
 ┃ ~ https://uptobox.com/f3m60u07mb5k
 ┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
-┇      *「 Kimi no Suizou wo Tabetai 」*
+┇   *「 Kimi no Suizou wo Tabetai 」*
 ┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
 ┃          Kualitas 480
 ┃ ~ https://mega.nz/file/1QozTaiT#DQ7rNo8WAE5ouWcUG3QBp67LizfcYuRy2QfQ_b4W33E
@@ -66,38 +69,52 @@ let handler = async m => m.reply(`
 ┃ ~ https://mega.nz/#!0A13iYpK!TZsGzvWzf6HRzX8pDQkvg6vHOTvqOaFOxG5I8-pKWjU
 ┃          Kualitas 1080
 ┃ ~ https://mega.nz/#!UUkjyaKY!sHrTiuMZG9xWSdjNQTXbi_JqdLaSTqg6jFVvwXpw-18
-┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
-┇      *「 Josee to Tora to Sakana-Tachi 」*
-┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
+┣ ┅ ━━━━━━━━━━━
+┇   *「 Josee to Tora to Sakana-Tachi 」*
+┣ ┅ ━━━━━━━━━━━
 ┃          Kualitas 480
 ┃ ~ https://uptobox.com/in1qafje5a99
 ┃          Kualitas 720
 ┃ ~ https://uptobox.com/e6j4srt0t8u6
 ┃          Kualitas 1080
 ┃ ~ https://uptobox.com/9ft1siwqimvg
-┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
-┇      *「 Kotonoha no Niwa 」*
-┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
+┣ ┅ ━━━━━━━━━━━
+┇   *「 Kotonoha no Niwa 」*
+┣ ┅ ━━━━━━━━━━━
 ┃          Kualitas 480
 ┃ ~ https://uptobox.com/3ut11uq8gxww
 ┃          Kualitas 720
 ┃ ~ https://uptobox.com/8sa2w7hsgfu3
-┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
-┇      *「 Hotarubi no Mori e 」*
-┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
+┣ ┅ ━━━━━━━━━━━
+┇   *「 Hotarubi no Mori e 」*
+┣ ┅ ━━━━━━━━━━━
 ┃          Kualitas 480
 ┃ ~ https://uptobox.com/k23nsxopex85
 ┃          Kualitas 720
 ┃ ~ https://uptobox.com/pp7pf3gl7lis
-┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
-┇      *「 Kyoukai no Kanata : I'll be Here 」*
-┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
+┣ ┅ ━━━━━━━━━━━
+┇   *「 Kyoukai no Kanata : I'll be Here 」*
+┣ ┅ ━━━━━━━━━━━
 ┃          Kualitas 480
 ┃ ~ https://uptobox.com/ep9s486r4rdp
 ┃          Kualitas 720
 ┃ ~ https://uptobox.com/mf4jjfvredfn
 ┗ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
-`.trim()) // Tambah sendiri kalo mau
+`.trim()
+  await conn.sendFile(m.chat, violet, 'VGarden.png', teks.trim(), { 
+      key: { 
+        remoteJid: 'status@broadcast', 
+        participant: '0@s.whatsapp.net', 
+        fromMe: false 
+      }, 
+      message: { 
+        "imageMessage": { 
+          "mimetype": "image/jpeg", 
+          "caption": `Link Anime Movie`,
+          }
+      }
+    } ,m)
+      }
 handler.help = ['anmovie']
 handler.tags = ['weebs']
 handler.command = /^anmovie$/i
