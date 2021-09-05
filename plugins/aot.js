375b7ecf0f9aa6a4d69755306eab8595a8d379fd
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-05 09:35:32 +0800

    Update aot.js

--- plugins/aot.js
+++ plugins/aot.js
@@ -1,4 +1,7 @@
-let handler = async m => m.reply(`
+let handler = async (m) => {
+  
+let violet =  './src/photo/Vi-chan.png'
+let teks = `
 ┏ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
 ┇          *「 Only Attack On Titan 」*
 ┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
@@ -54,8 +57,21 @@ let handler = async m => m.reply(`
 ┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
 ┃ ~ https://komikindo.co/manga/shingeki-no-kyojin-before-the-fall/
 ┗ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
-
-`.trim()) // Tambah sendiri kalo mau
+`.trim()
+  await conn.sendFile(m.chat, violet, 'Vi-chan.png', teks.trim(), { 
+      key: { 
+        remoteJid: 'status@broadcast', 
+        participant: '0@s.whatsapp.net', 
+        fromMe: false 
+      }, 
+      message: { 
+        "imageMessage": { 
+          "mimetype": "image/jpeg", 
+          "caption": `Link Anime Dan Manga Attack On Titan`,
+          }
+      }
+    } ,m)
+      }
 handler.help = ['aot']
 handler.tags = ['weebs']
 handler.command = /^aot$/i
