75d0b3d77a610e08ea1355c0b89c76cf775f1ab7
Hansky Pranata <90182023+Iluvhans@users.noreply.github.com>
2021-09-10 13:01:28 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 let handler = function (m) {
   //ini hanyalah contoh, jangan di uncomment_-
   //Example this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  this.sendContact(m.chat, '62887436568613', 'ブラック', m)
+  this.sendContact(m.chat, '6289516091066', 'Hans', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
