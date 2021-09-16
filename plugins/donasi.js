167b2753344904ad0d646a0c724ec7514602ef24
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-16 10:45:35 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,13 +1,29 @@
-let handler = async m => m.reply(`
-┏ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
-┇       *「 DONASI 」*
-┣ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
+let handler = async (m) => {
+  
+let violet =  './src/photo/faiitto.png'
+let teks = `
+┏━━ *「 DONASI 」*
+┣ ┅ ━━━━━━━━━━
+┃ ❖ Dana [0887436568613]
 ┃ ❖ OVO [0887436568613]
 ┃ ❖ Pulsa [0887436568613]
 ┗ ┅ ━━━━━━━━━━━━━━━━━━━━ ┅ ━
-
 Terimakasih sudah berdonasi 😁
-`.trim()) // Tambah sendiri kalo mau
+`.trim()
+  await conn.sendFile(m.chat, violet, 'faiitto.png', teks.trim(), { 
+      key: { 
+        remoteJid: 'status@broadcast', 
+        participant: '0@s.whatsapp.net', 
+        fromMe: false 
+      }, 
+      message: { 
+        "imageMessage": { 
+          "mimetype": "image/jpeg", 
+          "caption": `Donasi dan Pembayaran`,
+          }
+      }
+    } ,m)
+      }
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
