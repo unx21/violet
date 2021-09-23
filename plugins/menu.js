efd8c1604753cb65bdff300f939c3be645ac708e
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-23 11:13:07 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -84,7 +84,7 @@ ${'```%npmdesc```'}
 let handler = async (m, { conn, usedPrefix: _p }) => {
   try {
     let package = JSON.parse(await fs.promises.readFile(path.join(__dirname, '../package.json')).catch(_ => '{}'))
-    let violet = './src/photo/faiitto.png'
+    let violet = './src/photo/VioGarden.png'
     let unx = 'https://bit.ly/unxzx'
     //let premium = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
     let tnbot = (await conn.getFile(await conn.getProfilePicture(m.fromMe))).data.toString('base64')
@@ -182,7 +182,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     //conn.reply(m.chat, text.trim(), m)
-    await conn.sendFile(m.chat, violet, 'faiitto.png', text.trim(), { 
+    await conn.sendFile(m.chat, violet, 'VioGarden.png', text.trim(), { 
       key: { 
         remoteJid: 'status@broadcast', 
         participant: '0@s.whatsapp.net', 
