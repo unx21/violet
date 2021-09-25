c4e298ad8f44f62b670dc4aa844271ba2a196fcb
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-25 12:13:52 +0800

    Create notstonk.js

---
+++ plugins/notstonk.js
@@ -0,0 +1,24 @@
+const { sticker } = require('../lib/sticker')
+const { MessageType } = require('@adiwajshing/baileys')
+
+let handler = async (m, { conn, text }) => {
+  //await m.reply(global.wait)
+  try {
+    let stonk = global.API('https://hardianto.xyz', '/api/not-stonk', {
+    image: await conn.getProfilePicture(m.sender).catch(_ => ''),
+    apikey: 'hardianto'})
+    let stiker = await sticker(null, stonk, 'stonk', '@Kokoronationz × UNX')
+  conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+    quoted: m
+  })
+  } catch (e) {
+  m.reply('Conversion Failed')
+  }
+}
+handler.help = ['notstonk']
+handler.tags = ['sticker']
+handler.command = /^notstonk$/i
+handler.limit = true
+handler.group = false
+handler.register = true
+module.exports = handler
