c0cd74dba733a7869f6e1711de1664783a0a98ef
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-27 14:14:10 +0700

    Update _antiToxic.js

--- plugins/_antiToxic.js
+++ plugins/_antiToxic.js
@@ -7,7 +7,7 @@ handler.before = function (m, { user }) {
   let isGroupToxic = linkRegex.exec(m.text)
 
   if (!chat.antiToxic && isGroupToxic) {
-    m.reply('Jangan Toxic ya!!\n' + readMore + '\nMau Matikan? hubungi owner*')
+    m.reply('*[ ANTI TOXIC ]*\n\nJangan TOXIC wehhh!!\n' + readMore + '\nMau Matikan? hubungi owner*')
     if (global.opts['restrict']) {
       // if (!user.isAdmin) return true
       // this.groupRemove(m.chat, [m.sender])
