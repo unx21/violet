6323074a4fdd2cbc8d891338e51ec49d08cdadf5
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-28 13:39:26 +0800

    Update braintest_ans.js

--- plugins/braintest_ans.js
+++ plugins/braintest_ans.js
@@ -5,17 +5,17 @@ handler.before = async function (m) {
   let id = m.chat
   if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/「 Brain Test 」/i.test(m.quoted.text)) return
   conn.braintest = conn.braintest ? conn.braintest : {}
-  if (!(id in conn.braintest)) throw 'Soal itu telah berakhir!'
+  if (!(id in conn.braintest)) throw 'The question is over!'
   if (m.quoted.id == conn.braintest[id][0].id) {
     let json = JSON.parse(JSON.stringify(conn.braintest[id][1]))
     // m.reply(JSON.stringify(json, null, '\t'))
     if (m.text.toLowerCase() == json.jawaban.toLowerCase()) {
       global.DATABASE._data.users[m.sender].uang += conn.braintest[id][2]
-      m.reply(`*Benar!*\n+Rp${conn.braintest[id][2]}`)
+      m.reply(`*Correct!*\n+Rp${conn.braintest[id][2]}`)
       clearTimeout(conn.braintest[id][3])
       delete conn.braintest[id]
-    } else if (m.text.toLowerCase().endsWith(json.jawaban.split` `[1])) m.reply(`*Dikit Lagi!*`)
-    else m.reply(`*Salah!*`)
+    } else if (m.text.toLowerCase().endsWith(json.jawaban.split` `[1])) m.reply(`*Little More!*`)
+    else m.reply(`*Wrong!*`)
   }
 }
 handler.exp = 0
