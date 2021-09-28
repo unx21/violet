f21b56db961d53bca06353b1183f820e83264dec
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-28 22:18:57 +0800

    Update botchat4.js

--- plugins/botchat4.js
+++ plugins/botchat4.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn }) => {
 let sticker = './src/sticker/hehe.webp'
 conn.sendFile(m.chat, sticker, 'hehe.webp', null, m, true, {
 type: 'stickerMessage', // paksa tanpa convert di ffmpeg
-ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+ // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
 handler.customPrefix = /Hehe|hehe/
