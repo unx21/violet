611615ad8c9783650c61497c06d7cec00aa9cf2d
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-28 22:24:48 +0800

    Update botchat19.js

--- plugins/botchat19.js
+++ plugins/botchat19.js
@@ -5,9 +5,9 @@ let handler = async (m, { conn }) => {
 let sticker = './src/sticker/nhen.webp'
 conn.sendFile(m.chat, sticker, 'nhen.webp', null, m, true, {
 type: 'stickerMessage', // paksa tanpa convert di ffmpeg
-ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+ // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /Pedo|pedo/
+handler.customPrefix = /Pedo/
 handler.command = new RegExp
 module.exports = handler
