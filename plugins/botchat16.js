182aaf21a8fda7145c4a591e9e26dfee8fa84db0
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-28 22:23:33 +0800

    Update botchat16.js

--- plugins/botchat16.js
+++ plugins/botchat16.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn }) => {
 let sticker = './src/sticker/latom.webp'
 conn.sendFile(m.chat, sticker, 'latom.webp', null, m, true, {
 type: 'stickerMessage', // paksa tanpa convert di ffmpeg
-ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+ // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
 handler.customPrefix = /Latom|latom/
