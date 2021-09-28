1c2e8db710834981fe3dffe8a17991efacfa80ad
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-28 22:21:11 +0800

    Update botchat10.js

--- plugins/botchat10.js
+++ plugins/botchat10.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn }) => {
 let sticker = './src/sticker/sip.webp'
 conn.sendFile(m.chat, sticker, 'sip.webp', null, m, true, {
 type: 'stickerMessage', // paksa tanpa convert di ffmpeg
-ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+ // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
 handler.customPrefix = /siip|sip|sipp/
