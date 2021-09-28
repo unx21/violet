d5a194b05d44787597f8422692485b513975eb58
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-28 22:22:25 +0800

    Update botchat13.js

--- plugins/botchat13.js
+++ plugins/botchat13.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn }) => {
 let sticker = './src/sticker/wife.webp'
 conn.sendFile(m.chat, sticker, 'wife.webp', null, m, true, {
 type: 'stickerMessage', // paksa tanpa convert di ffmpeg
-ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+ // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
 handler.customPrefix = /huu istrinya kartun/
