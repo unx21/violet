6fafa1a291986e777596615ae3973afd2d94a28c
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-28 22:25:25 +0800

    Update botchat20.js

--- plugins/botchat20.js
+++ plugins/botchat20.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn }) => {
 let sticker = './src/sticker/umu.webp'
 conn.sendFile(m.chat, sticker, 'umu.webp', null, m, true, {
 type: 'stickerMessage', // paksa tanpa convert di ffmpeg
-ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+ // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
 handler.customPrefix = /Umu/
