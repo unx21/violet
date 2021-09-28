b0e8ad51f65549e1ff5043098e4dbb828d9470ed
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-28 22:23:09 +0800

    Update botchat15.js

--- plugins/botchat15.js
+++ plugins/botchat15.js
@@ -5,9 +5,9 @@ let handler = async (m, { conn }) => {
 let sticker = './src/sticker/yo.webp'
 conn.sendFile(m.chat, sticker, 'yo.webp', null, m, true, {
 type: 'stickerMessage', // paksa tanpa convert di ffmpeg
-ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+ // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /Halo|Hai|halo|hai|Yo|yoo/
+handler.customPrefix = /Halo|Hai|halo|hai/
 handler.command = new RegExp
 module.exports = handler
