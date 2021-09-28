f6ca08787cf727a3f21beaa62fff11594e8b0300
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-28 22:17:45 +0800

    Update botchat1.js

--- plugins/botchat1.js
+++ plugins/botchat1.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn }) => {
 let sticker = './src/sticker/pcs.webp'
 conn.sendFile(m.chat, sticker, 'pcs.webp', null, m, true, {
 type: 'stickerMessage', // paksa tanpa convert di ffmpeg
-ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+ // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
 handler.customPrefix = /onegai|arigatou|makasih|thanks/
