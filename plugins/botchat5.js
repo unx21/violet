c01a0d7ba1a3a28be4ab52fd6832c95d9df29cb2
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-28 22:19:17 +0800

    Update botchat5.js

--- plugins/botchat5.js
+++ plugins/botchat5.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn }) => {
 let sticker = './src/sticker/hmm.webp'
 conn.sendFile(m.chat, sticker, 'hmm.webp', null, m, true, {
 type: 'stickerMessage', // paksa tanpa convert di ffmpeg
-ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+ // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
 handler.customPrefix = /hmm/
