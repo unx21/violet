823108e978d14b9042bc8811a1d0ac898826ecc7
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-28 22:20:49 +0800

    Update botchat9.js

--- plugins/botchat9.js
+++ plugins/botchat9.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn }) => {
 let sticker = './src/sticker/nah.webp'
 conn.sendFile(m.chat, sticker, 'nah.webp', null, m, true, {
 type: 'stickerMessage', // paksa tanpa convert di ffmpeg
-ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
+ // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
 handler.customPrefix = /nahh|nah/
