1ab45fb7c5307bdecbf7676bb13527ae6bf4a491
Hansky Pranata <90182023+Iluvhans@users.noreply.github.com>
2021-09-28 22:18:34 +0700

    Create nwaifu.js
    Bacol?

---
+++ plugins/nwaifu.js
@@ -0,0 +1,31 @@
+let axios = require("axios")
+
+
+
+let handler = async (m, { conn }) => {
+
+   await m.reply('Tunggu bentar vio cariin... ')
+
+    let res = await axios("https://api.waifu.pics/nsfw/waifu")
+
+    let json = res.data
+
+    let url = json.url
+
+    conn.sendFile(m.chat, url, "waifu.png", "nyari bacol ya?", m)
+
+    }
+
+handler.help = ['nsfw waifu']
+
+handler.tags = ['internet']
+
+handler.command = /^nwaifu$/i
+
+handler.premium = true
+
+handler.register = true
+
+//Tenkyuu to Unx-sama
+
+module.exports = handler
