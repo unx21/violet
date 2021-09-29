1f05bd650f166370f5cbafaf6e03b781c099d957
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-29 14:11:25 +0800

    FREE APIKEY

--- config.js
+++ config.js
@@ -1,30 +1,29 @@
-/*
-* global.${text} fill in the text 
-*/
-global.owner = ['62887436568613'] // Put your number here
+global.owner = ['62887436568613', '62895361677059'] // Put your number here
 global.mods = [] // Want some help?
-global.prems = ['62887436568613'] // Premium user has unlimited limit
-global.wait = "[❗]  ```Wait a minute...```"
-global.eror = '```[ ⚠️ ] EROR 404```' 
-
+global.prems = ['62887436568613','6281387880724','6281233807317','62895398040685','62881023683976','6289516091066'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
-  neoxr: 'https://neoxr-api.herokuapp.com',
-  xteam: 'https://api.xteam.xyz'
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  pencarikode: 'https://pencarikode.xyz',
+  zekais: 'http://zekais-api.herokuapp.com',
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'APIKEYMU',
-  'https://neoxr-api.herokuapp.com': 'yntkts'
+  'https://api.xteam.xyz': '689330763f16fbcb',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.xyz': 'Haaachamaaa',
+  'https://pencarikode.xyz': 'pais',
+  'http://zekais-api.herokuapp.com': 'LjNKiMwk',
 }
 
 // Sticker WM
 global.packname = 'I hope you\'re fine'
 global.author = 'Violet-bot'
 
-global.multiplier = 89 // The higher, The harder levelup
-global.flatext = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&doScale=true&scaleWidth=800&scaleHeight=400&fontsize=90&fillTextType=1&text='
+global.multiplier = 69 // The higher, The harder levelup
 
 let fs = require('fs')
 let chalk = require('chalk')
