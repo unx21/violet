db1cb7e2aa7dcec16893405d0557411644c9d9b4
unxzx <84166927+unx21@users.noreply.github.com>
2021-11-04 17:43:30 +0800

    fix

--- plugins/suitpvp.js
+++ plugins/suitpvp.js
@@ -8,11 +8,11 @@ let poin_lose = -500
 let handler = async (m, { conn, usedPrefix }) => {
   conn.suit = conn.suit ? conn.suit : {}
   if (Object.values(conn.suit).find(room => room.id.startsWith('suit') && [room.p, room.p2].includes(m.sender))) throw 'Selesaikan suit mu yang sebelumnya'
-  if (!m.mentionedJid[0]) return m.reply(`_Siapa yang ingin kamu tantang?_\nTag orangnya.. Contoh\n\n${usedPrefix}suitpvp @${owner[0]}`, m.chat, { contextInfo: { mentionedJid: [owner[0] + '@s.whatsapp.net'] } })
+  if (!m.mentionedJid[0]) return m.reply(`_Siapa yang ingin kamu tantang?_\nTag orangnya.. Contoh\n\n${usedPrefix}suit @${owner[0]}`, m.chat, { contextInfo: { mentionedJid: [owner[0] + '@s.whatsapp.net'] } })
   if (Object.values(conn.suit).find(room => room.id.startsWith('suit') && [room.p, room.p2].includes(m.mentionedJid[0]))) throw `Orang yang kamu tantang sedang bermain suit bersama orang lain :(`
   let id = 'suit_' + new Date() * 1
   let caption = `
-     *「 SUIT PVP 」*
+      *「 SUIT PVP 」*
 
 @${m.sender.split`@`[0]} menantang @${m.mentionedJid[0].split`@`[0]} untuk bermain suit
 
@@ -32,7 +32,7 @@ Ketik "terima/ok/gas" untuk memulai suit\nKetik "tolak/gabisa/nanti" untuk menol
     }, timeout), poin, poin_lose, timeout
   }
 }
-handler.help = ['suitpvp <@user>', 'suit2 <@user>']
+handler.help = ['suitpvp <@user>', 'suit2 <@users>']
 handler.tags = ['games']
 handler.command = /^suit(pvp|2)$/i
 handler.limit = false
@@ -40,5 +40,3 @@ handler.group = true
 handler.register = true
 
 module.exports = handler
-
-// gapunya button :)
