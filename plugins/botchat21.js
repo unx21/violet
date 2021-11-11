07791c8f99834d3b2e2d604ce938026cb171923c
unxzx <84166927+unx21@users.noreply.github.com>
2021-11-11 22:33:21 +0800

    Create botchat21.js

---
+++ plugins/botchat21.js
@@ -0,0 +1,13 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let sticker = './src/sticker/deal.webp'
+conn.sendFile(m.chat, sticker, 'deal.webp', null, m, true, {
+type: 'stickerMessage', // paksa tanpa convert di ffmpeg
+ // true diatas ga work, sebab dipaksa tanpa convert ;v
+})
+}
+handler.customPrefix = /(D|d)eal(?)/
+handler.command = new RegExp
+module.exports = handler
