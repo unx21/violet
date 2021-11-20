c3c2fa39a85a9671c34c1e2e8522d743c2658d1f
unxzx <84166927+unx21@users.noreply.github.com>
2021-11-20 13:11:57 +0800

    Ngefigs

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -1,23 +1,23 @@
-let handler = m => m
-
-handler.all = async function (m) {
-    this.spam = this.spam ? this.spam : {}
-    if (m.sender in this.spam) {
-        this.spam[m.sender].count++
-        if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
-            if (this.spam[m.sender].count > 10) {
-                //global.DATABASE._data.users[m.sender].banned = true
-                m.reply('*Jangan Spam!!*')
+module.exports = {
+    async all(m) {
+        if (!m.message) return
+        this.spam = this.spam ? this.spam : {}
+        if (m.isBaileys && m.fromMe) return true
+        if (m.sender in this.spam) {
+            this.spam[m.sender].count++
+            if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
+                if (this.spam[m.sender].count > 10) {
+                    //global.DATABASE._data.users[m.sender].banned = true
+                    m.reply('*Jangan Spam!!*')
+                }
+                this.spam[m.sender].count = 0
+                this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
             }
-            this.spam[m.sender].count = 0
-            this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
+        }
+        else this.spam[m.sender] = {
+            jid: m.sender,
+            count: 0,
+            lastspam: 0
         }
     }
-    else this.spam[m.sender] = {
-        jid: m.sender,
-        count: 0,
-        lastspam: 0
-    }
 }
-
-module.exports = handler
