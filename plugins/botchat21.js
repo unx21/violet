22720c3c744e3cdd0a5b04c7b07501ef881098b2
unxzx <84166927+unx21@users.noreply.github.com>
2021-12-23 14:18:21 +0800

    Update botchat21.js

--- plugins/botchat21.js
+++ plugins/botchat21.js
@@ -8,6 +8,6 @@ type: 'stickerMessage', // paksa tanpa convert di ffmpeg
  // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /(D|d)eal(?)/
+handler.customPrefix = /(D|d)eal/
 handler.command = new RegExp
 module.exports = handler
